f24118676bc85dd33941475299b81be66c002f25
brightkingpin <luvandabright781@gmail.com>
2023-04-23 15:15:18 +0300

    Done with assignment

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,3 +1,4 @@
+/*
 import React, { useState } from "react";
 
 function Form() {
@@ -19,6 +20,43 @@ function Form() {
       <button type="submit">Submit</button>
     </form>
   );
+}
+
+export default Form;
+*/
+
+import React, { useState } from "react";
+
+function Form() {
+ const [firstName, setFirstName] = useState("John");
+ const [lastName, setLastName] = useState("Henry");
+ const [admin, setAdmin] = useState(false);
+
+ function handleFirstNameChange(event) {
+  setFirstName(event.target.value);
+ }
+
+ function handleLastNameChange(event) {
+  setLastName(event.target.value);
+ }
+
+ function handleAdminChange(event) {
+  setAdmin(event.target.checked);
+ }
+
+ function handleSubmit(event) {
+  event.preventDefault();
+  console.log({ firstName, lastName, admin });
+ }
+
+return (
+  <form onSubmit={handleSubmit}>
+    <input type="text" onChange={handleFirstNameChange} value={firstName} />
+    <input type="text" onChange={handleLastNameChange} value={lastName} />
+    <input type="checkbox" onChange={handleAdminChange} checked={admin} />
+    <button type="submit">Submit</button>
+  </form>
+);
 }
 
 export default Form;
